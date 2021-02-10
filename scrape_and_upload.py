5cd457e357988afbedbf28e2f49ae67cd32371fa
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2021-02-10 16:24:47 +0100

    mad ethe process more robust with intermediate saving

--- scrape_and_upload.py
+++ scrape_and_upload.py
@@ -5,6 +5,9 @@ Created on Sun Feb  7 13:39:47 2021
 @author: tglaubach
 """
 import pandas as pd
+import numpy as np
+
+import sqlite3
 
 import requests
 import os
@@ -55,25 +58,35 @@ def main(path, db_table=":memory:", links_register=None, host='localhost', port=
     #%%
 
     if os.path.exists(db_table):
+        print('LOADING FILETABLE: ' + db_table)
         ft = filetable(db_table)
     else:
+        print('CREATING NEW TABLE')
         ft = filetable()
 
     #%%
+
+    files_avail = set([str(s) for s in ft.df['file_id'].values])
 
     files = []
     for dirpath, dirnames, filenames in os.walk(path):
         for filename in [f for f in filenames if f.split('.')[-1] in accepted_filetypes]:
             s = os.path.join(dirpath, filename).replace("\\", "/")
             print('FOUND FILE: ' + s)
-            if s in ft:
+            if s in files_avail:
                 print('   already exists in filetable --> skipping')
             else:
                 print('   not found in filetable --> adding')
                 files.append(s)
             
             
-    
+    if links_register:
+        dms_register_df = pd.read_excel(links_register).dropna()
+        names_lnk_dc = {os.path.basename(ff):lnk for ff, lnk in zip(dms_register_df['Dateiname'], dms_register_df['Link'])}
+    else:
+        names_lnk_dc = {}
+
+
 
     def load(filename):
         tp = filename.split('.')[-1]
@@ -82,81 +95,96 @@ def main(path, db_table=":memory:", links_register=None, host='localhost', port=
             with open(filename, openstr) as fp:
                 pages_dc = loadfile(fp, tp, verbose=1)
                 return pages_dc
-        except FileNotFoundError:
+        except Exception as err:
+            print(f'ERROR while loading {err}... --> SKIPPING')
             return None
 
-    parallel = False
-    if parallel:
-        from joblib import Parallel, delayed
-        results = Parallel(n_jobs=-1, verbose=1, backend="threading")(
-                map(delayed(load), files))
-    else:
-        results = []
-        for i, filename in enumerate(files):
-            print(f'{i}/{len(files)}: {filename}')
-            results.append(load(filename))
-        
+    def extract_pages(filename, pages_dc, lnk):
+        pages_to_upload = []
+        if pages_dc is not None:
+            try:
+                
+                tp = filename.split('.')[-1]
+                for page_no, page_txt in pages_dc.items():
+                    tags = standard_preproc_txt(page_txt)
+                    
+                    pg = {  'location': os.path.dirname(filename),
+                            'file_name': os.path.basename(filename),
+                            'page_no': page_no,
+                            'n_pages': len(pages_dc),
+                            'tags': tags,
+                            'link': lnk,
+                            'raw_txt': page_txt
+                            }
+                    
+                    pages_to_upload.append(pg)
+
+            except Exception as err:
+                print(f"ERROR! {i} had an error while processing error msg: {err}. SKIPPING!")
+            
+        return pages_to_upload
+
 
     pages_to_upload = []
-    for i, (pages_dc, filename) in enumerate(zip(results, files)):
-        if pages_dc is None:
-            continue
-        try:
-            print(f'{i}/{len(files)}: {filename}')
-            tp = filename.split('.')[-1]
-            ft.add(filename, pages_dc, file_type=tp)
-            for page_no, page_txt in pages_dc.items():
-                tags = standard_preproc_txt(page_txt)
-                
-                pg = {  'location': os.path.dirname(filename),
-                        'file_name': os.path.basename(filename),
-                        'page_no': page_no,
-                        'n_pages': len(pages_dc),
-                        'tags': tags.split(),
-                        'raw_txt': page_txt
-                        }
-                
-                pages_to_upload.append(pg)
+    for i, filename in enumerate(files):
+        print(f'{i}/{len(files)}: {filename}')
 
-        except Exception as err:
-            print(f"ERROR! {i} had an error while processing error msg: {err}. SKIPPING!")
+        pages_dc = load(filename)
+        lnk = names_lnk_dc[filename] if filename in names_lnk_dc else ''
 
+        if pages_dc is not None:
+            try:
+                ft.add(filename, pages_dc, file_type=filename.split('.')[-1])
+                pgs_new = extract_pages(filename, pages_dc, lnk)
+                pages_to_upload += pgs_new
+            except Exception as err:
+                print('ERROR while extracting ' + str(err) + ' ... --> SKIPPING')
+        else:
+            print('ERROR while loading... --> SKIPPING')
 
+        if i % 10 == 0:
+            print('INTERMEDIATE SAVING')
+            ft.save(db_table)
+            df_for_storage = pd.DataFrame.from_records(pages_to_upload)
+            df_for_storage.to_sql(name='indextable', con=sqlite3.connect(db_table), if_exists='replace')
 
+    print('FINAL SAVING')
+    ft.save(db_table)
+    df_for_storage = pd.DataFrame.from_records(pages_to_upload)
+    df_for_storage.to_sql(name='indextable', con=sqlite3.connect(db_table), if_exists='replace')
+
+        
     #%%
 
     backup = json.dumps(pages_to_upload)
     pages_to_upload = json.loads(backup)
 
     #%%
-    if links_register:
-        dms_register_df = pd.read_excel(links_register).dropna()
-        names_lnk_dc = {os.path.basename(ff):lnk for ff, lnk in zip(dms_register_df['Dateiname'], dms_register_df['Link'])}
-    else:
-        names_lnk_dc = {}
 
-    #%%
+    res = es.search(index=category, body = {
+        'size' : 10000,
+        'query': {
+            'match_all' : {}
+        }
+    })
 
-    for i, page in enumerate(pages_to_upload):
-        lnk = names_lnk_dc[page['file_name']] if page['file_name'] in names_lnk_dc else ''
-        pages_to_upload[i]['link'] = lnk
-        if isinstance(pages_to_upload[i]['tags'], list):
-            pages_to_upload[i]['tags'] = ' '.join(pages_to_upload[i]['tags'])
-        
-    #%%
+    mkpth = lambda pg: 'PAGE_{}_{}'.format(pg['page_no'], os.path.join(pg['location'], pg['file_name']).replace('\\', '/'))
 
-    df_for_storage = pd.DataFrame.from_records(pages_to_upload)
-    df_for_storage.to_sql(name='indextable', con=sqlite3.connect(db_table), if_exists='replace')
-
-    #%%
-
-
+    id_dc = {mkpth(r['_source']):int(r['_id']) for r in  res['hits']['hits']}
+    
     #%%
 
     for i, pg in enumerate(pages_to_upload):
         print('{}/{}'.format(i, len(pages_to_upload)))
+        fname = mkpth(pg)
+
+        if fname in id_dc:
+            idnow = id_dc[fname]
+        else:
+            idnow = np.max(id_dc.values()) + 1
+            id_dc[idnow] = fname
         # s = "ctx._source.new_field = '{}'".format(pg['tags'])
-        es.index(id=i, index=category, doc_type=doc_type, body=pg)
+        es.index(id=idnow, index=category, doc_type=doc_type, body=pg)
 
 
 if __name__ == "__main__":
